27e2bb9e9d3e716bf2f5ee4f4fdfcc5e58c8cfb2
majomavizben <115664122+majomavizben@users.noreply.github.com>
2023-05-02 14:27:55 +0200

    Add files via upload

---
+++ Dokumentumtar/js/login_script.js
@@ -0,0 +1,38 @@
+function validalas(){
+  var username = document.getElementById("userName").value;
+  var password = document.getElementById("userPassword").value;
+  var usernameReg = /^[a-zA-Z0-9]+$/;
+  var passwordReg = /^[a-zA-Z0-9]+$/;
+  
+
+  if (username == ""){
+    alert("A felhasználónév mező nem maradhat üresen!");
+  }
+
+  else if (password == "")
+  {
+    alert("A jelszó mező nem maradhat üresen!");
+  }
+   /* 
+  else if(!usernameReg.test(username))
+  {
+    alert("A felhasználónév nem felel meg a feltételeknek!");
+  }
+  else if(!passwordReg.test(password))
+  {
+    alert("A jelszó nem felel meg a feltételeknek!");
+  }
+  */
+  else if(username == window.sessionStorage.getItem('ssUsername') && password == window.sessionStorage.getItem('ssPassword')){
+    window.location.replace("../html/index.html");
+    return false;
+    } 
+  
+}
+
+document.write('<p>Session store: ');
+		if (window.sessionStorage.getItem('ssUsername')){
+		document.write(window.sessionStorage.getItem('ssUsername'));
+	
+}
+document.write('</p>');
