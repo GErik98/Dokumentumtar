f6f1467a25d3bf941d9ec43b3b4e77a2bf21a1c2
majomavizben <115664122+majomavizben@users.noreply.github.com>
2023-05-02 14:43:59 +0200

    Delete Dokumentumtar directory

--- Dokumentumtar/js/mappa.js
+++
@@ -1,4 +0,0 @@
-/*---------------------MAPPA.HTML MAPPA NÉV--------------------------------*/
-const mappanev = document.getElementById('mappanev');
-mappanev.innerHTML =    window.sessionStorage.getItem('ssUsername')+"'s Documents/"
-                        +window.sessionStorage.getItem('ssMappa');
